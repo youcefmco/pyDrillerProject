6d4f178d697ae17b65b0fd537b3365a53147ff57
youcefmco <youcefmco@live.com>
2025-07-09 16:31:50 +0100

    my first pydriller project

---
+++ src/TestPydriller.py
@@ -0,0 +1,21 @@
+#Per‑commit granularity
+from pydriller import Repository
+
+for commit in Repository(
+        'C:/Users/youce/OneDrive/Documents/GitHub/PapyrusProject/'
+    ).traverse_commits():
+    print(
+        commit.hash,
+        commit.author.name,
+        commit.insertions,
+        commit.deletions
+    )
+
+#Per‑file granularity
+for commit in Repository(
+        r'C:/Users/youce/OneDrive/Documents/GitHub/PapyrusProject'
+    ).traverse_commits():
+    print("Commit:", commit.hash)
+    for m in commit.modified_files:
+        path = m.new_path or m.old_path
+        print(f"  {path}: +{m.added_lines} -{m.deleted_lines}")
